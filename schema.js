abdd44fc7b658315effa358b423fe88798752dfb
Sherman <shc410@gmail.com>
2017-08-01 03:13:07 -0700

    finish adding mutations \ud83c\udf86

--- schema.js
+++ schema.js
@@ -7,7 +7,7 @@ const {
   GraphQLNonNull
 } = require('graphql')
 const axios = require('axios')
-const mockDBUrl = 'http://localhost:3000';
+const mockDBUrl = 'http://localhost:3000/customers'
 
 // CustomerType
 const CustomerType = new GraphQLObjectType({
@@ -20,6 +20,7 @@ const CustomerType = new GraphQLObjectType({
   })
 })
 
+// queries
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -28,16 +29,65 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: {type: GraphQLString}
       },
-      resolve(parentValue, args) {
-        return axios.get(`${mockDBUrl}/customers/${args.id}`)
+      resolve (parentValue, args) {
+        return axios.get(`${mockDBUrl}/${args.id}`)
           .then(res => res.data)
           .catch(e => console.error(e))
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
-      resolve(parentValue, args) {
-        return axios.get(`${mockDBUrl}/customers`)
+      resolve (parentValue, args) {
+        return axios.get(`${mockDBUrl}`)
+          .then(res => res.data)
+          .catch(e => console.error(e))
+      }
+    }
+  }
+})
+
+// mutations
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        email: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)}
+      },
+      resolve (parentValue, args) {
+        return axios.post(mockDBUrl, {
+          name: args.name,
+          email: args.email,
+          age: args.age
+        })
+        .then(res => res.data)
+        .catch(e => console.error(e))
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve (parentValue, args) {
+        return axios.delete(`${mockDBUrl}/${args.id}`)
+          .then(res => res.data)
+          .catch(e => console.error(e))
+      }
+    },
+    updateCustomer: {
+      type: CustomerType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt}
+      },
+      resolve (parentValue, args) {
+        return axios.patch(`${mockDBUrl}/${args.id}`, args)
           .then(res => res.data)
           .catch(e => console.error(e))
       }
@@ -46,5 +96,6 @@ const RootQuery = new GraphQLObjectType({
 })
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
-});
+  query: RootQuery,
+  mutation
+})
